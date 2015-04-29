231a1dab6058145a82e5fb682ac7361e5d1ba5ae
mchenryc <mchenryc@gmail.com>
2015-04-29 14:12:04 -0400

    whitespace cleanup

--- chrome/content/managefolders.js
+++ chrome/content/managefolders.js
@@ -1,7 +1,7 @@
-var ManageFolders = (function() 
+var ManageFolders = (function()
 {
   var folderNodeId;
-  
+
   var MF = {
     init : function MF_init() {
       var contextMenu = document.getElementById("placesContext");
@@ -20,25 +20,25 @@ var ManageFolders = (function()
         document.getElementById("placesContext_manageFolder").hidden = !isFolder;
       }
     },
-    
+
     showOrganizer : function MF_showOrganizer(event) {
       // check to see if organizer already open or open it. Needs to stay consistent
       // with the actual window opening calls found in browser-places.js
       // http://mxr.mozilla.org/firefox/source/browser/base/content/browser-places.js#544
       var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
       var organizer = wm.getMostRecentWindow("Places:Organizer");
-      
+
       function selectFolder() {
         // The tree object, aka organizer.PlacesOrganizer._places (vai places.js)
         var places = organizer.document.getElementById("placesList");
-	if (places) {
-	    places.selectItems([folderNodeId]);
-	    if (places.currentIndex)
-		places.treeBoxObject.ensureRowIsVisible(places.currentIndex);
-	    places.focus();
-	}
+        if (places) {
+          places.selectItems([folderNodeId]);
+          if (places.currentIndex)
+            places.treeBoxObject.ensureRowIsVisible(places.currentIndex);
+          places.focus();
+        }
       }
-      
+
       if (!organizer) {
         // once organizer window is loaded, postpone folder selection until after
         // the organizer init method is executed (see places.js)
@@ -46,8 +46,8 @@ var ManageFolders = (function()
           setTimeout(selectFolder, 1);
           organizer.removeEventListener("load", selectOnLoad, false);
         }
-        
-        organizer = openDialog("chrome://browser/content/places/places.xul", 
+
+        organizer = openDialog("chrome://browser/content/places/places.xul",
                                "", "chrome,toolbar=yes,dialog=no,resizable");
         organizer.addEventListener("load", selectOnLoad, false);
       }
